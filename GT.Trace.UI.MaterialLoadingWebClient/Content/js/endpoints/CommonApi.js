a4025bfff5c22cd2ebc8ec5dd095dda9240a2a85
Rogelio Arriaga <r.arriaga@generaltransmissions.com>
2024-12-18 16:54:57 -0600

    fix httprequest updateLineBom in MaterialLoadingWebClient

--- GT.Trace.UI.MaterialLoadingWebClient/Content/js/endpoints/CommonApi.js
+++ GT.Trace.UI.MaterialLoadingWebClient/Content/js/endpoints/CommonApi.js
@@ -5,6 +5,7 @@
             httpGet(`${apiUrl}/api/lines/${lineCode}/etis/${partNo}`, onSuccess, onFailure),
 
         updateLineBom: (partNo, lineCode, onSuccess) =>
-            httpPut(`${apiUrl}/api/lines/updategama/partno/${partNo}/lineCode/${lineCode}`, onSuccess),
+        //httpPut(`${apiUrl}/api/lines/updategama/partno/${partNo}/lineCode/${lineCode}`, onSuccess),
+            httpPut(`${apiUrl}/api/lines/updategama/partno/${partNo}/${partNo}/lineCode/${lineCode}/${lineCode}`, onSuccess),
     };
 })('http://mxsrvapps.gt.local/gtt/services/common');
